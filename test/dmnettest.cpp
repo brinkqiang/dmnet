c7b8a92997b511fa8aa4b1846026e27137e34450
brinkqiang <brinkqiang@users.noreply.github.com>
2018-09-30 20:55:36 +0800

    Update dmnettest.cpp

--- test/dmnettest.cpp
+++ test/dmnettest.cpp
@@ -5,6 +5,6 @@
 #include "dmformat.h"
 
 int main( int argc, char* argv[] ) {
-	std::cout << fmt::format("{0} {1}", "hello world! ", DMFormatDateTime());
+    std::cout << fmt::format("{0} {1}", "hello world! ", DMFormatDateTime());
     return 0;
 }
